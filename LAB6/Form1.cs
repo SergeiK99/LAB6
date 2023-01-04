1dff7820c27ea159ad970768b8ff5525e659cfd7
SergeiK99 <skx2013@mail.ru>
2023-01-05 05:29:11 +0700

    Дело сделано

--- LAB6/Form1.cs
+++ LAB6/Form1.cs
@@ -43,99 +43,133 @@ namespace LAB6
         {
             chart1.Series.Clear();
         }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pb1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text=="" & textBox2.Text == "" & textBox3.Text == "")
-            {
-                MessageBox.Show("Вы не выбрали значение A,B,C!");
-                return;
-            }
-            if (textBox1.Text == "" )
-            {
-                MessageBox.Show("Вы не выбрали значение A");
-                return;
-            }
-            if (textBox2.Text == "")
-            {
-                MessageBox.Show("Вы не выбрали значение B");
-                return;
-            }
-            if (textBox3.Text == "")
-            {
-                MessageBox.Show("Вы не выбрали значение C");
-                return;
-            }
-            eq = new QuadEquation ( a: Convert.ToInt32(textBox1.Text), b: Convert.ToInt32(textBox2.Text), c: Convert.ToInt32(textBox3.Text));
-        }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            if (eq==null)
+            int index = comboBox1.SelectedIndex;
+            if (index==0)
+            {
+                if (textBox1.Text == "" & textBox2.Text == "" & textBox3.Text == "")
+                {
+                    MessageBox.Show("Вы не выбрали значение A,B,C!");
+                    return;
+                }
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("Вы не выбрали значение A");
+                    return;
+                }
+                if (textBox2.Text == "")
+                {
+                    MessageBox.Show("Вы не выбрали значение B");
+                    return;
+                }
+                if (textBox3.Text == "")
+                {
+                    MessageBox.Show("Вы не выбрали значение C");
+                    return;
+                }
+                eq = new QuadEquation(a: Convert.ToInt32(textBox1.Text), b: Convert.ToInt32(textBox2.Text), c: Convert.ToInt32(textBox3.Text));
+            }
+            if (index==1)
+            {
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("Вы не выбрали значение A!");
+                    return;
+                }
+                eq = new SinEquation(a: Convert.ToInt32(textBox1.Text));
+            }
+            if (index==-1)
             {
                 MessageBox.Show("Вы не выбрали тип уравнения!");
                 return;
             }
+
             int N;
             int.TryParse(textBox4.Text, out N);
+            if (!Int32.TryParse(textBox4.Text, out N))
+            {
+                MessageBox.Show("Некорректное значение количества разбиений");
+                return;
+
+            }
+            Clear();
             DrawFunction(x1: Convert.ToInt32(textBox5.Text),x2: Convert.ToInt32(textBox6.Text),eq,N: Convert.ToInt32(textBox4.Text));
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "")
-            {
-                MessageBox.Show("Вы не выбрали значение A!");
-                return;
-            }
-            eq = new SinEquation(a: Convert.ToInt32(textBox1.Text));
-        }
 
         private void button6_Click(object sender, EventArgs e)
         {
             Clear();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void button7_Click(object sender, EventArgs e)
         {
-            int N;
 
+            int index = comboBox1.SelectedIndex;
+            if (index == 0)
+            {
+                if (textBox1.Text == "" & textBox2.Text == "" & textBox3.Text == "")
+                {
+                    MessageBox.Show("Вы не выбрали значение A,B,C!");
+                    return;
+                }
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("Вы не выбрали значение A");
+                    return;
+                }
+                if (textBox2.Text == "")
+                {
+                    MessageBox.Show("Вы не выбрали значение B");
+                    return;
+                }
+                if (textBox3.Text == "")
+                {
+                    MessageBox.Show("Вы не выбрали значение C");
+                    return;
+                }
+                eq = new QuadEquation(a: Convert.ToInt32(textBox1.Text), b: Convert.ToInt32(textBox2.Text), c: Convert.ToInt32(textBox3.Text));
+            }
+            if (index == 1)
+            {
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("Вы не выбрали значение A!");
+                    return;
+                }
+                eq = new SinEquation(a: Convert.ToInt32(textBox1.Text));
+            }
+            if (index == -1)
+            {
+                MessageBox.Show("Вы не выбрали тип уравнения!");
+                return;
+            }
+
+            int N;
+            int index2 = comboBox2.SelectedIndex;
             if (!Int32.TryParse(textBox4.Text, out N))
             {
                 MessageBox.Show("Некорректное значение количества разбиений");
                 return;
 
             }
-            RectagleIntegrator Intgr = new RectagleIntegrator();
-            MessageBox.Show($"{Intgr.ToString()}: {Intgr.Integrate(eq, Convert.ToInt32(textBox5.Text), Convert.ToInt32(textBox6.Text), Convert.ToInt32(textBox4.Text))}");
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            int N;
-
-            if (!Int32.TryParse(textBox4.Text, out N))
+            if (index2==-1)
             {
-                MessageBox.Show("Некорректное значение количества разбиений");
-                return;
+                MessageBox.Show("Не выбран метод интегрирования!");
 
             }
-            TRIntegrator Intgr = new TRIntegrator();       
-            MessageBox.Show($"{Intgr.ToString()}: {Intgr.Integrate(eq, x1: Convert.ToInt32(textBox5.Text), x2: Convert.ToInt32(textBox6.Text), N: Convert.ToInt32(textBox4.Text))}");
+            if (index2==0)
+            {
+                RectagleIntegrator Intgr = new RectagleIntegrator();
+                MessageBox.Show($"{Intgr.ToString()}: {Intgr.Integrate(eq, Convert.ToInt32(textBox5.Text), Convert.ToInt32(textBox6.Text), Convert.ToInt32(textBox4.Text))}");
+            }
+            if (index2==1)
+            {
+                TRIntegrator Intgr = new TRIntegrator();
+                MessageBox.Show($"{Intgr.ToString()}: {Intgr.Integrate(eq, x1: Convert.ToInt32(textBox5.Text), x2: Convert.ToInt32(textBox6.Text), N: Convert.ToInt32(textBox4.Text))}");
+            }
         }
     }
 }
